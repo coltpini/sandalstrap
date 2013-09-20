d46b4e2cc8482896bec9a873b78f0b63d80923a5
coltpini <coltpini@gmail.com>
2013-09-20 11:26:14 -0600

    IE9 SUPPORT!!!! BU YA!!!!!

--- js/sandlestrap.js
+++ js/sandlestrap.js
@@ -44,18 +44,14 @@ SandleStrap.prototype.updateStyle = function(){
 	style.appendChild(document.createTextNode(loadingSelector + loadingStyle));
 	style.appendChild(document.createTextNode(loadingSelector + hideStyle));
 	style.appendChild(document.createTextNode(loadingSelector + globalStyle));
+	this.loadingSelector = loadingSelector;
 };
 var ieStinks = false;
-SandleStrap.prototype.inserted = function(e){
-	if(!ieStinks){
-		console.log('IE STINKS!!');
-		ieStinks = true;
+SandleStrap.prototype.inserted = function(e,elem){
+	if (e) {
+		elem = e.target;
 	}
-	if (e.animationName == "nodeInserted") {
-		var elem = e.target;
-		this.inject(elem);
-		e.target.addClass('rendered');
-	}
+	this.inject(elem);
 };
 
 SandleStrap.prototype.ssChangeAttribute = function(attr,val){
@@ -84,55 +80,59 @@ SandleStrap.prototype.ssInnerHTML = function(html){
 };
 
 SandleStrap.prototype.inject = function(elem){
-	if(!elem.containsClass('rendered')){
+	if(elem.isFW && !elem.containsClass('rendered')){
 		var tagName = elem.tagName.toLowerCase(),
 			obj = this.elements[tagName],
 			children = [],
 			i,
 			tempElem = fw('<div>');
-		elem.ssChangeAttribute = this.ssChangeAttribute;
-		elem.ssInnerHTML = this.ssInnerHTML;
-		var template = obj.config.template;
-		var matches = obj.config.attributes;
-		if(matches){
-			i = matches.length;
-			while(i--){
-				if(matches[i] === 'type'){
-					template = obj.templates[elem.getAttribute(matches[i])] || template;
+		if(obj){
+			elem.ssChangeAttribute = this.ssChangeAttribute;
+			elem.ssInnerHTML = this.ssInnerHTML;
+			var template = obj.config.template;
+			var matches = obj.config.attributes;
+			if(matches){
+				i = matches.length;
+				while(i--){
+					if(matches[i] === 'type'){
+						template = obj.templates[elem.getAttribute(matches[i])] || template;
+					}
+				}
+				matches.push('content');
+				// TODO LATER: I might need to unescape the {{}} if this is being used with a framework like angular or handlebars. 
+				// This shouldn't be a problem because you need to register your attributes. But I want to keep this in here for a note to be sure to test it.
+				i = matches.length;
+				while(i--){
+					var attr = matches[i],
+						reg = new RegExp("{{" + attr + "}}", 'g'),
+						attrReg = new RegExp('="{{' + attr + '}}"', 'g');
+					if(attrReg.test(template) && elem.getAttribute(attr)){
+						template = template.replace(attrReg,'="' + elem.getAttribute(attr) + '"');
+					}
+					else {
+						var replacement = elem.getAttribute(attr) || "";
+						template = template.replace(reg,'<span class="' + tagName + '-' + attr + '">' + replacement + '</span>');
+					}
 				}
 			}
-			matches.push('content');
-			// TODO LATER: I might need to unescape the {{}} if this is being used with a framework like angular or handlebars. 
-			// This shouldn't be a problem because you need to register your attributes. But I want to keep this in here for a note to be sure to test it.
-			i = matches.length;
-			while(i--){
-				var attr = matches[i],
-					reg = new RegExp("{{" + attr + "}}", 'g'),
-					attrReg = new RegExp('="{{' + attr + '}}"', 'g');
-				if(attrReg.test(template) && elem.getAttribute(attr)){
-					template = template.replace(attrReg,'="' + elem.getAttribute(attr) + '"');
-				}
-				else {
-					var replacement = elem.getAttribute(attr) || "";
-					template = template.replace(reg,'<span class="' + tagName + '-' + attr + '">' + replacement + '</span>');
-				}
-			}
-		}
 
-		while(elem.childNodes.length > 0){
-			var cn = elem.childNodes[0];
-			tempElem.appendChild(cn);
-			children.push(cn);
-		}
-		elem.innerHTML = template;
-		if(template.indexOf('<content>') !== -1){
-			var cont = elem.find('[content]')[0];
-			for (i = 0; i < children.length; i++) {
-				cont.appendChild(children[i]);
+			while(elem.childNodes.length > 0){
+				var cn = elem.childNodes[0];
+				tempElem.appendChild(cn);
+				children.push(cn);
 			}
+			elem.innerHTML = template;
+			if(template.indexOf('<content>') !== -1){
+				var cont = elem.find('[content]')[0];
+				for (i = 0; i < children.length; i++) {
+					cont.appendChild(children[i]);
+				}
+			}
+			if(obj.config.init)
+				obj.config.init(elem);
+
+			elem.addClass('rendered');
 		}
-		if(obj.config.init)
-			obj.config.init(elem);
 	}
 };
 
@@ -141,31 +141,48 @@ var sandlestrap = new SandleStrap();
 // Here we are going to listen for insertion via animation listeners. This way we can inject right away without waiting for document.ready.
 // Substitution for mutation events.
 
-var didAnimationStart = false,
+var insertedProxy = fw.proxy(sandlestrap.inserted, sandlestrap),
 	listener = ["animationstart","MSAnimationStart","oanimationstart","webkitAnimationStart"],
-	i = listener.length;
+	i = listener.length,
+	animationListenerSupport = false;
+
 while(i--){
-	//var elem = fw('<div>');
-	//if("on" + listener[i] in elem){
-		document.addEventListener(listener[i], fw.proxy(sandlestrap.inserted, sandlestrap), false);
-		//didAnimationStart = true;
-	//	break;
-	//}
-	//else {
-
-	//}
+	if(fw.isEventSupported(listener[i])){
+		document.addEventListener(listener[i], insertedProxy, false);
+		animationListenerSupport = true;
+	}
 }
-//console.log(i);
+//This should only be used if the animation event doesn't work.
+var observeDOM = function(obj, callback){
+	var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+    if( MutationObserver ){
+        // define a new observer
+        var obs = new MutationObserver(function(mutations, observer){
+            if( mutations[0].addedNodes.length){
+				var i = mutations[0].addedNodes.length;
+				while(i--){
+					callback(undefined, mutations[0].addedNodes[i]);
+				}
+			}
+        });
+        // have the observer observe foo for changes in children
+        obs.observe( obj, { childList:true, subtree:true });
+    }
+    else
+		obj.addListener('DOMNodeInserted', callback);
+};
 
-
-// var el = document.createElement(TAGNAMES[eventName] || 'div');
-// eventName = 'on' + eventName;
-// var isSupported = (eventName in el);
-// if (!isSupported) {
-// el.setAttribute(eventName, 'return;');
-// isSupported = typeof el[eventName] == 'function';
-// }
-// el = null;
+// Observe a specific DOM element, only run if animationStart event doesn't work
+if(!animationListenerSupport)
+	observeDOM( document.documentElement , insertedProxy);
 
 //set up the content element
 document.createElement('content');
+
+document.onreadystatechange = function (){
+	if (document.readyState == "complete") {
+		fw('[' + sandlestrap.loadingSelector + ']').forEach(function(elem){
+			insertedProxy(undefined, elem);
+		});
+	}
+};
